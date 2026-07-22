c3386c95155841a176f0fffc5dc71a1df7888d84
Priyam2773 <priyamrai282@gmail.com>
2026-07-23 01:29:00 +0530

    add booking functionality with create, get, and cancel endpoints

---
+++ server/routes/bookingRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { protect } from "../middlewares/auth.js";
+import { cancleBooking, createBooking, getMyBooking } from "../controllers/bookingController.js";
+
+const bookingRouter = Router();
+
+bookingRouter.post("/", protect, createBooking)
+bookingRouter.get("/my", protect, getMyBooking)
+bookingRouter.put("/:id/cancle", protect, cancleBooking)
+
+export default bookingRouter;
