1b25154493fbb6db1694eb167396071eb6c75096
Priyam Rai <priyamrai282@gmail.com>
2026-07-31 16:02:58 +0530

    \fix: deploy compiled JavaScript server bundle directly"

---
+++ server/dist/models/user.js
@@ -0,0 +1,16 @@
+import mongoose, { model, Schema } from 'mongoose';
+const UserSchema = new Schema({
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+    password: { type: String, required: true, minlength: 6 },
+    phone: { type: String, trim: true, minlength: 6 },
+    role: { type: String, enum: ["user", "admin", "owner"], default: "user" },
+}, { timestamps: true });
+// Schema password when converting to JSON
+UserSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
+    }
+});
+export const User = (mongoose.models && mongoose.models.User) ? mongoose.models.User : model("User", UserSchema);
