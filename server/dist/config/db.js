28fff5a0bc60bead5a0c370745feef8203a3d52c
Priyam Rai <priyamrai282@gmail.com>
2026-07-31 14:44:13 +0530

    updated

---
+++ server/dist/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+    mongoose.connection.on("connected", () => console.log("MongoDB Connected"));
+    await mongoose.connect(process.env.MONGO_URI);
+};
+export default connectDB;
