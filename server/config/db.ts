ed8e13208372772043f2e1e467fb6b1639111c29
Priyam Rai <priyamrai282@gmail.com>
2026-07-31 16:01:06 +0530

    fix: add connection error event listener to prevent serverless crash on db connection failure

--- server/config/db.ts
+++ server/config/db.ts
@@ -5,6 +5,9 @@ const connectDB = async () => {
         return;
     }
     mongoose.connection.on("connected", () => console.log("MongoDB Connected"));
+    mongoose.connection.on("error", (err) => {
+        console.error("MongoDB Connection Error:", err);
+    });
     await mongoose.connect(process.env.MONGO_URI!);
 }
 
